cb1a5af9a28d25dfa6ed52bab042afc94d165a9d
CreziPlay <118505360+CreziPlay@users.noreply.github.com>
2023-09-22 22:13:21 +0700

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,27 @@
+function OpenInfo(){
+    document.getElementById("me").style.display = "none";
+
+
+
+}
+
+function OpenMore(){
+    document.getElementById("me").style.display = "none";
+
+}
+function OpenAbout(){
+    document.getElementById("me").style.display = "none";
+
+
+}
+function OpenContact(){
+    document.getElementById("me").style.display = "none";
+
+
+}
+
+
+$(function(){
+	$('.m').fadeOut(0);
+
+});
